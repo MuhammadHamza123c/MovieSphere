e7d6372892172b6b6493e319d8753deb97902d41
Muhammad Hamza <muhammadhamzao241@gmail.com>
2026-06-05 09:09:05 +0500

    Update client.js

--- MovieSphere_backend/frontend/src/api/client.js
+++ MovieSphere_backend/frontend/src/api/client.js
@@ -1,6 +1,8 @@
 import axios from 'axios'
 
-const client = axios.create()
+const client = axios.create({
+  baseURL: import.meta.env.VITE_API_URL || ''
+})
 
 client.interceptors.request.use(config => {
   const token = localStorage.getItem('msp_token')
